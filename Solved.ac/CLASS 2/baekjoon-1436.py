eb9ecf084a06562d80759bbb60fcf7953d576768
jangdawit <jangdawit@inha.edu>
2021-01-20 22:01:58 +0900

    Algorithm

---
+++ Solved.ac/CLASS 2/baekjoon-1436.py
@@ -0,0 +1,17 @@
+n = int(input())
+temp = 666
+while True:
+    strTemp = str(temp)
+    for i in range(len(strTemp)-2):
+        if strTemp [i]== '6':
+            if strTemp[i+1] == '6':
+                if strTemp[i+2] == '6':
+                    n -= 1
+                    if n == 0:
+                        print(temp)
+                        exit()
+                    break
+            else:
+                
+    
+    temp += 1
