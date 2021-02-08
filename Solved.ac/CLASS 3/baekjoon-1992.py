d2a4d447b0e4112dcd2e8a346af20b3729700e12
david02324 <jangdawit@inha.edu>
2021-02-08 20:09:03 +0900

    Algorithm

--- Solved.ac/CLASS 3/baekjoon-1992.py
+++ Solved.ac/CLASS 3/baekjoon-1992.py
@@ -1,3 +1,4 @@
+# dawitblog.tistory.com
 from sys import stdin
 input = stdin.readline
 
@@ -5,10 +6,12 @@ video = [list(input()) for _ in range(int(input()))]
 
 def yaho(video,startR,startC,n):
     global answer
+    # 1X1 정사각형까지 분할되었을 경우
     if n == 1:
         answer += video[startR][startC]
         return
 
+    # 좌상단 기억
     zeroOrOne = video[startR][startC]
     
     for r in range(startR,startR+n):
