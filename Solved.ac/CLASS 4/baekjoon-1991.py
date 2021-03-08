924905a4b81f42d829c5c1c5357bd02a81d974d3
david02324 <jangdawit@inha.edu>
2021-03-09 03:15:20 +0900

    트리 순회 - 재귀

---
+++ Solved.ac/CLASS 4/baekjoon-1991.py
@@ -0,0 +1,46 @@
+from sys import stdin
+input = stdin.readline
+
+dic = {}
+def preOrder(node):
+    global answer
+
+    answer += node
+    if dic[node][0] != '.':
+        preOrder(dic[node][0])
+    if dic[node][1] != '.':
+        preOrder(dic[node][1])
+
+def inOrder(node):
+    global answer
+
+    if dic[node][0] != '.':
+        inOrder(dic[node][0])
+    answer += node
+    if dic[node][1] != '.':
+        inOrder(dic[node][1])
+
+def postOrder(node):
+    global answer
+
+    if dic[node][0] != '.':
+        postOrder(dic[node][0])
+    if dic[node][1] != '.':
+        postOrder(dic[node][1])
+    answer += node
+
+for _ in range(int(input())):
+    conn = input().split()
+    dic[conn[0]] = (conn[1],conn[2])
+
+answer = ''
+preOrder('A')
+print(answer)
+
+answer = ''
+inOrder('A')
+print(answer)
+
+answer = ''
+postOrder('A')
+print(answer)
